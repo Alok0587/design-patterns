4e15dfa8e76b1c3b87203e7df89c9ede79e69dfd
Alok <alok.kumar@bhn.com>
2025-03-19 22:31:40 +0530

    refactoring

--- Creational/src/main/java/com/creational/CreationalApplication.java
+++ Creational/src/main/java/com/creational/CreationalApplication.java
@@ -1,10 +1,9 @@
-package com.example.creational;
+package com.creational;
 
 import com.creational.builder.classic.ConcreteOrderBuilder;
 import com.creational.builder.classic.Order;
 import com.creational.builder.classic.OrderBuilderDirector;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@SpringBootApplication
 public class CreationalApplication {
